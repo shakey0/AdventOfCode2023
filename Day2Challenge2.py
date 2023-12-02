a1e4748aeba0cf4eae691c5274704076cee177b5
Andrew <shakeyshakespeare0@gmail.com>
2023-12-02 10:11:22 +0000

    Day 2 challenge 2 complete

--- Day2Challenge2.py
+++ Day2Challenge2.py
@@ -1,37 +1,33 @@
 from Day2Input import puzzle_input
 
-def find_possible_games(games_record, red=None, green=None, blue=None):
+def find_possible_games(games_record):
     
     divided_lines = games_record.split('\n')
 
-    possible_games = []
+    games = []
     for line in divided_lines:
-        game_number = int(line.split(': ')[0].split(' ')[1])
         sets = line.split(': ')[1]
         divided_sets = sets.split('; ')
-        set_valid = True
+        found_red_cubes, found_green_cubes, found_blue_cubes = 0, 0, 0
         for set in divided_sets:
             divided_set = set.split(', ')
             for item in divided_set:
                 if 'red' in item:
                     red_cubes = int(item.split(' ')[0])
-                    if red:
-                        if red_cubes > red:
-                            set_valid = False
+                    if red_cubes > found_red_cubes:
+                        found_red_cubes = red_cubes
                 elif 'green' in item:
                     green_cubes = int(item.split(' ')[0])
-                    if green:
-                        if green_cubes > green:
-                            set_valid = False
+                    if green_cubes > found_green_cubes:
+                        found_green_cubes = green_cubes
                 elif 'blue' in item:
                     blue_cubes = int(item.split(' ')[0])
-                    if blue:
-                        if blue_cubes > blue:
-                            set_valid = False
-        if set_valid:
-            possible_games.append(game_number)
+                    if blue_cubes > found_blue_cubes:
+                        found_blue_cubes = blue_cubes
+        power_of_set = found_red_cubes * found_green_cubes * found_blue_cubes
+        games.append(power_of_set)
     
-    return sum(possible_games)
+    return sum(games)
 
 
 test_string = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -40,5 +36,5 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''
 
-print(find_possible_games(test_string, 12, 13, 14))
-print(find_possible_games(puzzle_input, 12, 13, 14))
+print(find_possible_games(test_string))
+print(find_possible_games(puzzle_input))
